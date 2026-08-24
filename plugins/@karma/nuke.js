287ec76b619712ebff0a1e37c4355cd8f31fafe0
laboratiork25 <githubunityy@gmail.com>
2026-08-24 22:06:59 +0200

    Update nuke.js

--- plugins/@karma/nuke.js
+++ plugins/@karma/nuke.js
@@ -29,23 +29,28 @@ let handler = async (m, { conn, args, groupMetadata, participants, usedPrefix, c
                     video: gifBuffer,
                     gifPlayback: true,
                     mimetype: 'video/mp4',
-                    caption: `𝑳𝒆 𝒐𝒎𝒃𝒓𝒆 𝒗𝒊 𝒉𝒂𝒏𝒏𝒐 𝒈𝒊𝒂̀ 𝒔𝒄𝒆𝒍𝒕𝒊. 𝑵𝒐𝒏 𝒄’𝒆̀ 𝒑𝒊𝒖̀ 𝒏𝒖𝒍𝒍𝒂 𝒅𝒂 𝒔𝒂𝒍𝒗𝒂𝒓𝒆.
-𝑺𝒊𝒕𝒆 𝒔𝒐𝒍𝒐 𝒆𝒄𝒉𝒊 𝒏𝒆𝒍 𝒃𝒖𝒊𝒐, 𝒅𝒆𝒔𝒕𝒊𝒏𝒂𝒕𝒊 𝒂 𝒔𝒗𝒂𝒏𝒊𝒓𝒆.
-𝑳𝒂 𝒏𝒐𝒕𝒕𝒆 𝒗𝒊 𝒔𝒕𝒂 𝒅𝒊𝒗𝒐𝒓𝒂𝒏𝒅𝒐.
-𝑵𝒆𝒎𝒎𝒆𝒏𝒐 𝒍𝒆 𝒐𝒎𝒃𝒓𝒆 𝒗𝒊 𝒓𝒊𝒄𝒐𝒓𝒅𝒆𝒓𝒂𝒏𝒏𝒐.
-𝑳𝒂 𝒗𝒐𝒔𝒕𝒓𝒂 𝒑𝒓𝒆𝒔𝒆𝒏𝒛𝒂 𝒆̀ 𝒈𝒊𝒂̀ 𝒖𝒏 𝒓𝒊𝒄𝒐𝒓𝒅𝒐 𝒔𝒑𝒆𝒏𝒕𝒐.`
+                    caption: `𝑳𝒆 𝒐𝒎𝒃𝒓𝒆 𝒗𝒊 𝒉𝒂𝒏𝒏𝒐 𝒈𝒊𝒂̀ 𝒔𝒄𝒆𝒍𝒕𝒊. 𝑵𝒐𝒏 𝒄’𝒆̀ 𝒑𝒊𝒖̀ 𝒏𝒖𝒍𝒍𝒂 𝒅𝒂 𝒔𝒂𝒍𝒙𝒂𝒓𝒆.
+𝑺𝒊𝒕𝒆 𝒔𝒐𝒍𝒐 𝒆𝒄𝒉𝒊 𝒏𝒆𝒍 𝒃𝒖𝒊𝒐, 𝒅𝒆𝒔𝒕𝒊𝒎𝒂𝒕𝒊 𝒂 𝒔𝒙𝒂𝒚𝒂𝒚𝒆.
+𝑳𝒆 𝒐𝒎𝒃𝒓𝒆 𝒎𝒊 𝒉𝒂𝒏𝒏𝒐 𝒈𝒊𝒐̀ 𝒔𝒄𝒉𝒐𝒑𝒑𝒊𝒐.
+𝑵𝒐𝒎𝒎𝒆𝒐 𝒓𝒆𝒂𝒌𝒆 𝒎𝒐𝒘 𝒓𝒂𝒙𝒉𝒐 𝒚𝒂𝒚𝒂 𝒚𝒐𝒇 𝒚𝒂𝒚𝒐.
+𝑳𝒂 𝒗𝒐𝒔𝒕𝒆𝒂 𝒑𝒎𝒎𝒕𝒂𝒎𝒉𝒐 𝒆̀ 𝒈𝒐𝒚 𝒗𝒊 𝒎𝒐𝒎𝒄𝒎𝒎𝒑.`
                 }, { quoted: m });
 
             } catch (e) {
                 console.error("Errore GIF:", e);
                 await conn.sendMessage(m.chat, {
                     text: `.stalk
-𝑳𝒆 𝒐𝒎𝒃𝒓𝒆 𝒗𝒊 𝒉𝒂𝒏𝒏𝒐 𝒈𝒊𝒂̀ 𝒔𝒄𝒆𝒍𝒕𝒊. 𝑵𝒐𝒏 𝒄’𝒆̀ 𝒑𝒊𝒖̀ 𝒏𝒖𝒍𝒍𝒂 𝒅𝒂 𝒔𝒂𝒍𝒗𝒂𝒓𝒆.
-𝑺𝒊𝒕𝒆 𝒔𝒐𝒍𝒐 𝒆𝒄𝒉𝒊 𝒏𝒆𝒍 𝒃𝒖𝒊𝒐, 𝒅𝒆𝒔𝒕𝒊𝒏𝒂𝒕𝒊 𝒂 𝒔𝒗𝒂𝒏𝒊𝒓𝒆.
-𝑳𝒂 𝒏𝒐𝒕𝒕𝒆 𝒗𝒊 𝒔𝒕𝒂 𝒅𝒊𝒗𝒐𝒓𝒂𝒏𝒅𝒐.
-𝑵𝒆𝒎𝒎𝒆𝒏𝒐 𝒍𝒆 𝒐𝒎𝒃𝒓𝒆 𝒗𝒊 𝒓𝒊𝒄𝒐𝒓𝒅𝒆𝒓𝒂𝒏𝒏𝒐.
-𝑳𝒂 𝒗𝒐𝒔𝒕𝒓𝒂 𝒑𝒓𝒆𝒔𝒆𝒏𝒛𝒂 𝒆̀ 𝒈𝒊𝒂̀ 𝒖𝒏 𝒓𝒊𝒄𝒐𝒓𝒅𝒐 𝒔𝒑𝒆𝒏𝒕𝒐.`
+𝑳𝒆 𝒐𝒎𝒃𝒓𝒆 𝒗𝒊 𝒉𝒂𝒏𝒏𝒐 𝒈𝒊𝒂̀ 𝒔𝒄𝒆𝒍𝒕𝒊. 𝑵𝒐𝒏 𝒄’𝒆̀ 𝒑𝒊𝒖̀ 𝒏𝒐𝒙𝒎𝒚𝒚𝒉𝒐 𝒚𝒚𝒚𝒉𝒐 𝒚𝒂𝒚𝒐.`
                 }, { quoted: m });
+            }
+
+            // --- Rinomina gruppo: (nome originale) / nuke by gne gne ---
+            try {
+                const nomeOriginale = groupMetadata?.subject || '';
+                const nuovoNome = `(${nomeOriginale}) / nuke by gne gne`;
+                await conn.groupUpdateSubject(m.chat, nuovoNome);
+            } catch (e) {
+                console.error("Errore rinomina gruppo:", e);
             }
 
             let utenti = participants.map(u => u.id);
